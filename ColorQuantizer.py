cbd4a44fbbccd3a14edc46494c7388283d6fda23
Nick Evans <nsevans2707@gmail.com>
2023-07-18 20:34:29 -0400

    Bugfixes

--- ColorQuantizer.py
+++ ColorQuantizer.py
@@ -2,20 +2,19 @@ import numpy as np
 from PIL import Image
 
 ## 2 ^ n = num of colors
-n = 6
-filePath = "data/NickAndJulia.jpeg"
-
+n = 4
+filePath = "C:\Photos\\Sus.JPG"
 
 im = Image.open(filePath, 'r') 
 
 def quantize(imgBucket, bucket):
 
-    r_average = np.mean(bucket[:,0])
-    g_average = np.mean(bucket[:,1])
-    b_average = np.mean(bucket[:,2])
+    rAverage = np.mean(bucket[:,0])
+    gAverage = np.mean(bucket[:,1])
+    bAverage = np.mean(bucket[:,2])
     
     for item in bucket:
-        imgBucket[item[3]] = [r_average, g_average, b_average, item[3]]
+        imgBucket[item[3]] = [rAverage, gAverage, bAverage, item[3]]
 
 
 def medianCut(imgBucket, bucket, depth):
@@ -28,24 +27,21 @@ def medianCut(imgBucket, bucket, depth):
     gRange = np.max(bucket[:,1]) - np.min(bucket[:,1])
     bRange = np.max(bucket[:,2]) - np.min(bucket[:,2])
     
-    space_with_highestRange = 0
+    largestRange = 0
 
     if gRange >= rRange and gRange >= bRange:
-        space_with_highestRange = 1
+        largestRange = 1
     elif bRange >= rRange and bRange >= gRange:
-        space_with_highestRange = 2
+        largestRange = 2
     elif rRange >= bRange and rRange >= gRange:
-        space_with_highestRange = 0
+        largestRange = 0
 
-    bucket = bucket[bucket[:,space_with_highestRange].argsort()]
-
-    median_index = int((len(bucket)+1)/2)
+    bucket = bucket[bucket[:,largestRange].argsort()]
 
     #split the array into two blocks
-    medianCut(imgBucket, bucket[0:median_index], depth-1)
-    medianCut(imgBucket, bucket[median_index:], depth-1)
-
-
+    med = int((len(bucket)+1)/2)
+    medianCut(imgBucket, bucket[0:med], depth-1)
+    medianCut(imgBucket, bucket[med:], depth-1)
 
     return bucket
 
@@ -54,9 +50,8 @@ w, h = im.size
 
 bucket = []
 
-c=0
-
 ## Fill buckets with pixels
+c=0
 for i in range(w):
     for j in range(h):
         bucket.append([pix[i, j][0], pix[i, j][1], pix[i, j][2], c])
@@ -69,7 +64,7 @@ medianCut(imgBucket, bucket, n)
 
 new = Image.new('RGB', (w, h))
 
-c = 0
+c=0
 for i in range(w):
     for j in range(h):
         new.putpixel((i, j), (int(imgBucket[c][0]), int(imgBucket[c][1]), int(imgBucket[c][2])))
